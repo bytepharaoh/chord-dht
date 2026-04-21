29131ced9bfbea560976a7a02e17412f700b62e2
kage-ops-dev <e.maiorovinn@gmail.com>
2026-04-21 21:56:54 +0300

    feat: implement finger construction logic

---
+++ chord/finger_table.py
@@ -0,0 +1,16 @@
+from bisect import bisect_left
+
+def build_finger_table(node, ring):
+    node.finger_table = []
+    
+    for i in range(node.m):
+        target_id = (node.node_id + 2**i) % (2**node.m)
+        
+        idx = bisect_left(ring, target_id)
+        
+        if idx == len(ring):
+            successor_node = ring[0]
+        else:
+            successor_node = ring[idx]
+            
+        node.finger_table.append(successor_node)
